33ba471d14b76c91afd5d94537491465fbfa7b5c
Izam Mohammed <106471909+izam-mohammed@users.noreply.github.com>
2023-12-11 16:24:59 +0530

    Update stage_05_model_evaluation.py

--- src/sentimentAnalysis/pipeline/stage_05_model_evaluation.py
+++ src/sentimentAnalysis/pipeline/stage_05_model_evaluation.py
@@ -21,9 +21,9 @@ class ModelEvaluationTrainingPipeline:
         None
         """
         config = ConfigurationManager()
-        model_trainer_config = config.get_model_evaluation_config()
-        model_trainer_config = ModelEvaluation(config=model_trainer_config)
-        model_trainer_config.evaluate()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.evaluate()
 
 
 if __name__ == "__main__":
